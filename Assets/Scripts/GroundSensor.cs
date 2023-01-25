eb9b8f22f0ddc16f5aab55290d3b009725bb6524
NineNero <salmaurbano01@gmail.com>
2023-01-25 11:06:28 +0100

    Animatons

--- Assets/Scripts/GroundSensor.cs
+++ Assets/Scripts/GroundSensor.cs
@@ -4,11 +4,24 @@ using UnityEngine;
 
 public class GroundSensor : MonoBehaviour
 {
+   private PlayerControler controller;
    public bool isGrounded;
+   
+   void Awake()
+   {
+       controller = GetComponentInParent<PlayerControler>();
+   }
    
    void OnTriggerEnter2D(Collider2D other)
    {
        isGrounded = true;
+       controller.anim.SetBool("IsJumping", false);
+   }
+
+  void OnTriggerStay2D(Collider2D other)
+   {
+       isGrounded = true;
+       controller.anim.SetBool("IsJumping", false);
    }
 
    void OnTriggerExit2D(Collider2D other)
