54db50c2f01ce1a0d83092f71051c00eb0e79d25
NineNero <salmaurbano01@gmail.com>
2023-02-22 11:17:18 +0100

    Menu

--- Assets/Scripts/Money.cs
+++ Assets/Scripts/Money.cs
@@ -4,12 +4,22 @@ using UnityEngine;
 
 public class Money : MonoBehaviour
 {
+    public AudioClip coinSound;
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             Destroy(gameObject);
+            source.PlayOneShot(coinSound);
         }
+    
     }
-
+    
+        private AudioSource source;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        source = GetComponent<AudioSource>();
+    }
 }
